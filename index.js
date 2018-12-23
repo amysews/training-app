803d2d0e04db8c02024e21cab1f40eb2c1e81446
Amy Collin <amy@zaptic.com>
2018-12-23 15:25:25 +0000

    add psql database

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 express()
   .get('/', (req, res) => res.send('hello'))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.json(results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
